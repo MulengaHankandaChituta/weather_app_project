4bb5142199b180c857c0b94a6b84c05ba9b89848
MulengaHankandaChituta <mulengahankandachituta@gmail.com>
2025-08-16 14:15:15 +0200

    Configure STATIC_ROOT for collectstatic on PythonAnywhere

--- weather_app/wsgi.py
+++ weather_app/wsgi.py
@@ -1,16 +1,13 @@
-"""
-WSGI config for weather_app project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/5.2/howto/deployment/wsgi/
-"""
-
 import os
+import sys
 
-from django.core.wsgi import get_wsgi_application
+# Add your project directory to the Python path
+project_path = '/home/mulenga/weather_app_project'
+if project_path not in sys.path:
+    sys.path.append(project_path)
 
+# Set the Django settings module
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'weather_app.settings')
 
+from django.core.wsgi import get_wsgi_application
 application = get_wsgi_application()
