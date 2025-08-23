554b206013746fbc8e1d70c71ddc3d08024ea07a
MulengaHankandaChituta <mulengahankandachituta@gmail.com>
2025-08-23 19:07:14 +0200

    Configure Whitenoise for static files and secure API key
    - Installed Whitenoise and added it to requirements.txt
    - Updated settings.py to use WhiteNoise middleware and static file storage
    - Removed print statements exposing OPENWEATHER_API_KEY
    - Collected static files for production deployment

--- weather_app/settings.py
+++ weather_app/settings.py
@@ -41,6 +41,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'weather_app.urls'  
@@ -89,6 +90,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 # Media files
